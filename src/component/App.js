fc506e517cf82b551745b09df024ee717af5461e
dhruv50ae <linkdp2012@gmail.com>
2022-04-27 20:35:55 +0100

    Added connect

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { selectSong } from "../actions";
+import SongList from "./SongList";
+import {connect} from react-redux
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
-export default App;
+export default connect()(SongList)
