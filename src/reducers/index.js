532b16d96f28673c3036cbe28a66c30a793710c2
dhruv50ae <linkdp2012@gmail.com>
2022-04-27 20:33:44 +0100

    Added redux

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+const songsReducers = () => {
+  return [
+    { title: "The only thing they fear is you", duration: "7:42" },
+    { title: "BFG 10000", duration: "3:47" },
+    { title: "Cultist base", duration: "2:54" },
+    { title: "Mastermind", duration: "2:52" },
+  ];
+};
+
+const selectedSongReducers = (selectedSong = null) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducers,
+  selectedSong: selectedSongReducers,
+});
